81645fddd20251a301aeb411af24200cb6ce5ad3
devkakaroto <sergiocad2018@gmail.com>
2021-10-20 17:11:57 -0600

    Commit3: Created a ProductDetailActivity.kt where the product specification and details will be shown. Start the configuration of this screen and added the BACK button in the top.

--- app/src/main/java/com/arquitetoandroid/appcommerce/MainActivity.kt
+++ app/src/main/java/com/arquitetoandroid/appcommerce/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.arquitetoandroid.appcommerce
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val productItem: LinearLayout = findViewById(R.id.ll_product_item)
+        productItem.setOnClickListener {
+             val intent: Intent = Intent(this, ProductDetailActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/com/arquitetoandroid/appcommerce/ProductDetailActivity.kt
@@ -0,0 +1,30 @@
+package com.arquitetoandroid.appcommerce
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.arquitetoandroid.appcommerce.databinding.ActivityProductDetailBinding
+
+class ProductDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProductDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProductDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+
+}
